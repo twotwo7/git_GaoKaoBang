343d3f62f991e9831a74e18fdc22a533cf28000b
He Hao <hehaopro@163.com>
2019-04-27 09:32:28 +0800

    添加了sqlhelper，以及sqldemo

--- gaokaobang/lib/main.dart
+++ gaokaobang/lib/main.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 import 'package:gaokaobang/mainpage.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+import 'tools/SqlHelper.dart';
 void main() {
     debugPaintSizeEnabled = false;
     runApp(new MyApp());
@@ -9,8 +12,31 @@ void main() {
     SystemChrome.setSystemUIOverlayStyle(systemUiOverlayStyle);
 }
 class MyApp extends StatelessWidget {
+  void sqldemo() async{
+    SqlHelper sqlhelper=new SqlHelper();
+    var databasesPath = await getDatabasesPath();
+    String path = join(databasesPath, 'demo.db');
+    String SqlCreate='CREATE TABLE IF NOT EXISTS Test (id INTEGER PRIMARY KEY, name TEXT, value INTEGER, num REAL)';
+    await sqlhelper.open(path, SqlCreate);
+
+    String tablename='Test';
+    var valuesmap = <String, dynamic>{
+      'name': 'hehao',
+      'value': 27,
+      'num':456.789
+    };
+    int id=await sqlhelper.insert(tablename,valuesmap);
+    print(id);
+
+    String SqlQuery='SELECT * FROM Test WHERE id=$id';
+    List<Map> list=await sqlhelper.query(SqlQuery);
+    print(list);
+
+    sqlhelper.close();
+  }
   @override
   Widget build(BuildContext context) {
+    sqldemo();
     return new MaterialApp(
       title: '高考帮',
       theme: new ThemeData(
---
+++ gaokaobang/lib/tools/SqlHelper.dart
@@ -0,0 +1,45 @@
+import 'package:sqflite/sqflite.dart';
+/*
+* 使用方法具体参考main.dart中的sqldemo
+*
+* 增加其他方法可以参考https://pub.dartlang.org/packages/sqflite#-readme-tab-
+* */
+class SqlHelper{
+  Database db;
+  /*
+  //使用的时候记得在函数前加上await
+  var databasesPath = await getDatabasesPath();
+  String path = join(databasesPath, 'demo.db');
+  SqlCreate='CREATE TABLE IF NOT EXISTS Test (id INTEGER PRIMARY KEY, name TEXT, value INTEGER, num REAL)';
+  */
+  Future open(String path,String SqlCreate) async {
+    db = await openDatabase(path, version: 1,
+        onCreate: (Database db, int version) async {
+          await db.execute(SqlCreate);
+        });
+  }
+  /*
+  String tablename='Test';
+   var valuesmap = <String, dynamic>{
+      'name': 'hehao',
+      'value': 27,
+      'num':456.789
+    };
+  * */
+  Future<int> insert(String tablename,Map<String,dynamic> valuesmap) async {
+    int id=-1;
+    if(db==null)return id;
+    id=await db.insert(tablename, valuesmap);
+    return id;
+  }
+
+  /*
+  * SqlQuery='SELECT * FROM Test';
+  * */
+  Future<List> query(String SqlQuery) async{
+    List<Map> list = await db.rawQuery(SqlQuery);
+    return list;
+  }
+
+  Future close() async => db.close();
+}
